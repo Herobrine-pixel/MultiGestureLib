8b6c2c39aa8ba13da29c532d88e9a5bd96bf6d42
Herobrine Sah <ashasah77@gmail.com>
2025-10-12 09:11:23 +0530

    Create FullDemo.ino

---
+++ examples/FullDemo/FullDemo.ino
@@ -0,0 +1,34 @@
+#include <MultiGestureLib.h>
+
+MultiGestureLib gestures;
+
+void setup() {
+  Serial.begin(9600);
+
+  // Sensor setup (same as in main.cpp)
+  gestures.addUltrasonic(2, 3);
+  gestures.addUltrasonic(4, 5);
+  gestures.addUltrasonic(6, 7);
+  gestures.addUltrasonic(8, 9);
+  gestures.addCapacitive(12);
+
+  gestures.setThreshold(20);
+  gestures.setSwipeTimeout(500);
+  gestures.setHoldTime(800);
+  gestures.setHoverTime(1500);
+
+  // Gesture callbacks
+  gestures.onGesture(SWIPE_LEFT, [](){ Serial.println("Swipe Left"); });
+  gestures.onGesture(SWIPE_RIGHT, [](){ Serial.println("Swipe Right"); });
+  gestures.onGesture(SWIPE_UP, [](){ Serial.println("Swipe Up"); });
+  gestures.onGesture(SWIPE_DOWN, [](){ Serial.println("Swipe Down"); });
+  gestures.onGesture(TAP, [](){ Serial.println("Tap"); });
+  gestures.onGesture(HOLD, [](){ Serial.println("Hold"); });
+  gestures.onGesture(HOVER, [](){ Serial.println("Hover"); });
+  gestures.onGesture(CIRCLE_CW, [](){ Serial.println("Circle CW"); });
+  gestures.onGesture(CIRCLE_CCW, [](){ Serial.println("Circle CCW"); });
+}
+
+void loop() {
+  gestures.update();
+}
